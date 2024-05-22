6be081f149a0f2d95cb2c5c1a7c3ea4407d85720
Grand <grand.uk@tutanota.com>
2024-05-22 13:35:27 +0100

    remove openvrpath config from home manager

--- common/home.nix
+++ common/home.nix
@@ -88,24 +88,24 @@
     };
   };
 
-  xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-    {
-      "config": :
-      [
-        "${config.xdg.dataHome}/Steam/config"
-      ],
-      "external_drivers" : null,
-      "jsonid" : "vrpathreg",
-      "log" :
-      [
-        "${config.xdg.dataHome}/Steam/logs"
-      ],
-      "runtime" :,
-      [
-        "${pkgs.opencomposite}/lib/opencomposite"
-      ],
-      "version" : 1
-    }
-    '';
+  # xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+  #   {
+  #     "config": :
+  #     [
+  #       "${config.xdg.dataHome}/Steam/config"
+  #     ],
+  #     "external_drivers" : null,
+  #     "jsonid" : "vrpathreg",
+  #     "log" :
+  #     [
+  #       "${config.xdg.dataHome}/Steam/logs"
+  #     ],
+  #     "runtime" :,
+  #     [
+  #       "${pkgs.opencomposite}/lib/opencomposite"
+  #     ],
+  #     "version" : 1
+  #   }
+  #   '';
   
 }

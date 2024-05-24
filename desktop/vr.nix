e8ef094d9659b5834d8ad53d2566fd7c500edbf2
Grand <grand.uk@tutanota.com>
2024-05-24 11:46:52 +0100

    enables nixpkgs-xr

--- desktop/vr.nix
+++ desktop/vr.nix
@@ -1,5 +1,7 @@
 { config, pkgs, inputs, ...}: {
   # Envison stuff to go here
+  nixpkgs.xr.enable = true;
+
   environment.systemPackages = [
     inputs.envision.packages.x86_64-linux.envision
     pkgs.wlx-overlay-s

2c1fa1561a8d14100bd4b69f79fb00e5ecb82f85
Grand <grand.uk@tutanota.com>
2024-10-04 18:25:24 +0100

    added vesktop flatpak to config

--- common/flatpak.nix
+++ common/flatpak.nix
@@ -1,4 +1,9 @@
 { config, pkgs, ...}: {
-  services.flatpak.enable = true;
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "dev.vencord.Vesktop"
+    ];
+  };
   xdg.portal.enable = true;
 }

ff7776f0675ef2acdbb7e098c8fe24bc9e7857ad
Grand <grand.uk@tutanota.com>
2025-01-05 21:08:51 +0000

    switch envision out for pure monado

--- desktop/vr.nix
+++ desktop/vr.nix
@@ -4,12 +4,16 @@
 #    inputs.envision.packages.x86_64-linux.envision
     inputs.nixpkgs-xr.packages.x86_64-linux.wlx-overlay-s
   ];
-  programs.envision = {
-    enable = true;
-  };
-  # services.monado = {
+  # programs.envision = {
   #   enable = true;
-  #   highPriority = true;
   # };
+  services.monado = {
+    enable = true;
+    highPriority = true;
+  };
+  systemd.user.services.monado.environment = {
+    STEAMVR_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+  };
   # SteamVR stuff to go here
 }

173151bb224101abcd4d0d995792df0af4b60cf3
Grand <grand.uk@tutanota.com>
2025-01-29 21:58:28 +0000

    fix use of refind module

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
         laptop = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [
-            refind.system.boot.loader.refind.refind
+            "${refind}/system/boot/loader/refind/refind.nix"
             ./laptop/hardware.nix
             ./common/common.nix
             ./laptop/core.nix

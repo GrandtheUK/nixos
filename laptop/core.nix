34a4835db5e21b6ed5ad943fe49e1347945a78da
Grand <grand.uk@tutanota.com>
2025-01-29 22:31:51 +0000

    disable grub

--- laptop/core.nix
+++ laptop/core.nix
@@ -1,5 +1,8 @@
 { config, pkgs, inputs, ...}: {
   imports = [];
   networking.hostName = "nix-laptop";
-  boot.loader.refind.enable = true;
+  boot.loader = {
+    grub.enable = false;
+    refind.enable = true;
+  };
 }

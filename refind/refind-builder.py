353e8ba9b1c4d65297def46dede7b0e7ed988860
Grand <grand.uk@tutanota.com>
2025-01-30 03:51:12 +0000

    fixed typo in refined builder

--- refind/refind-builder.py
+++ refind/refind-builder.py
@@ -190,7 +190,7 @@ def main():
                 if os.path.exists(themes_dir + "-backup"):
                     shutil.rmtree(themes_dir + "-backup")
                 os.rename(themes_dir, themes_dir + "-backup")
-            shutil.copytree(paths, refind_root)
+            shutil.copytree(path, refind_root)
     else:
         print(themes)
 

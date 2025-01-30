7b2df22b358f1daf09b15c3d6344636bc631d90d
Grand <grand.uk@tutanota.com>
2025-01-30 02:23:30 +0000

    removed swapdevice from laptop

--- laptop/hardware.nix
+++ laptop/hardware.nix
@@ -22,10 +22,7 @@
       fsType = "vfat";
     };
 
-  swapDevices = [{
-    device = "/swap/swapfile";
-    size = 10*1014;
-  }];
+  swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

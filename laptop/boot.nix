efd7a3330a135fd99fa1097775b15c0dc5fa703e
Grand <grand.uk@tutanota.com>
2025-01-31 13:22:09 +0000

    mistyped option

--- laptop/boot.nix
+++ laptop/boot.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, ...}: {
     boot = {
         loader = {
-            efiSysMountPoint = "/boot/efi";
+            efi.efiSysMountPoint = "/boot/efi";
 
             refind = {
                 enable = true;

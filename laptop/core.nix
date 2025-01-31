dad283e53e7cf4cfc50356f0b2fdcb646627b6b7
Grand <grand.uk@tutanota.com>
2025-01-31 21:52:19 +0000

    username config because syncthing broke

--- laptop/core.nix
+++ laptop/core.nix
@@ -11,6 +11,10 @@
     enable = true;
     openDefaultPorts = true;
     dataDir = "/home/ben/Documents";
+    settings.gui = {
+      user = "ben";
+      password = "";
+    };
   };
   
   system.stateVersion = "25.05";

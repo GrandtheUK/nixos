128a53ee0b6094310018a4c155c1f31c4b576b66
Grand <grand.uk@tutanota.com>
2025-02-01 13:12:40 +0000

    added device IDs for syncthing on laptop

--- laptop/core.nix
+++ laptop/core.nix
@@ -13,6 +13,12 @@
     dataDir = "/home/ben/Documents";
     configDir = "/home/ben/.config/syncthing";
     user = "ben";
+    settings = {
+      devices = {
+        "Desktop" = { id = "HKEKVOA-AZF7UMF-QNZPY3I-ZUT6XZU-GEYBUMM-HJCUT7L-6IBCGAF-JDLV4AJ"; };
+        "ZFold3" = { id = "2QH4QSF-SDJMZEO-RL5WNXD-NYU6TDN-AVCV3IV-FK7GHLW-6SD3D6J-7VHA5QN"; };
+      };
+    };
   };
   
   system.stateVersion = "25.05";

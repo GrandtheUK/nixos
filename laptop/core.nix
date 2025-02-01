438ec88527b1e55b2a1d7d9bd11b3ed524ae42c6
Grand <grand.uk@tutanota.com>
2025-02-01 13:25:41 +0000

    added TTRPG folder to syncthing

--- laptop/core.nix
+++ laptop/core.nix
@@ -18,6 +18,13 @@
         "Desktop" = { id = "HKEKVOA-AZF7UMF-QNZPY3I-ZUT6XZU-GEYBUMM-HJCUT7L-6IBCGAF-JDLV4AJ"; };
         "ZFold3" = { id = "2QH4QSF-SDJMZEO-RL5WNXD-NYU6TDN-AVCV3IV-FK7GHLW-6SD3D6J-7VHA5QN"; };
       };
+      folders = {
+        "9ax4a-smam2" = {
+          path = "/home/ben/Documents/TTRPG";
+          devices = [ "Desktop" "ZFold3" ];
+          ignorePerms = true;
+        };
+      };
     };
   };
   

ba94fb92c95e6655fa07db48979fa97d6553b293
Grand <grand.uk@tutanota.com>
2025-03-06 16:20:47 +0000

    added maliit-keyboard and qtvirtualkeyboard

--- common/kde.nix
+++ common/kde.nix
@@ -23,11 +23,14 @@
   environment.systemPackages = (with pkgs; [
       konsole
       xdg-desktop-portal
+      maliit-keyboard
+      maliit-framework
     ]) ++ (with pkgs.libsForQt5; [
       kate
       xdg-desktop-portal-kde
       polkit-kde-agent
       kcalc
+      qt5.qtvirtualkeyboard
     ]) ++ (with pkgs.kdePackages; [
       partitionmanager
     ]);
